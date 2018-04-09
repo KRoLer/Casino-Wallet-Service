841f748d3d3209a9f5c0e84145bc262af765071e
KRoLer <nikitin.dmytro@gmail.com>
2018-04-09 23:23:09 +0300

    Convert all possible responses to JSON

--- src/main/scala/com/casino/routes/Routes.scala
+++ src/main/scala/com/casino/routes/Routes.scala
@@ -11,10 +11,12 @@ import scala.util.{Failure, Success}
 
 case class Balance(balance: Double)
 case class User(playerId: Long, amount: Option[Double])
+case class GeneralError(msg: String = "Internal server error.")
 
 trait Protocols extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val balanceFormat = jsonFormat1(Balance.apply)
   implicit val userFormat = jsonFormat2(User.apply)
+  implicit val generalErrorFormat = jsonFormat1(GeneralError.apply)
 }
 
 class UnsecuredRoutes (service: WalletService) extends Protocols {
@@ -26,9 +28,10 @@ class UnsecuredRoutes (service: WalletService) extends Protocols {
         get {
           onComplete(service.balance(playerId)) {
             case Success(Right(balance)) => complete(StatusCodes.OK -> Balance(balance))
-            case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> msg)
+            case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> GeneralError(msg))
 
-            case Failure(error) => complete(StatusCodes.InternalServerError -> error)
+            case Success(_) => complete(StatusCodes.InternalServerError -> GeneralError())
+            case Failure(error) => complete(StatusCodes.InternalServerError -> GeneralError(error.getMessage))
           }
         }
       } ~
@@ -36,9 +39,10 @@ class UnsecuredRoutes (service: WalletService) extends Protocols {
           (post & entity(as[User])) { user =>
             onComplete(service.register(user.playerId)) {
               case Success(Right(balance)) => complete(StatusCodes.OK -> Balance(balance))
-              case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> msg)
+              case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> GeneralError(msg))
 
-              case Failure(error) => complete(StatusCodes.InternalServerError -> error)
+              case Success(_) => complete(StatusCodes.InternalServerError -> GeneralError())
+              case Failure(error) => complete(StatusCodes.InternalServerError -> GeneralError(error.getMessage))
             }
           }
         } ~
@@ -46,10 +50,11 @@ class UnsecuredRoutes (service: WalletService) extends Protocols {
           (post & entity(as[User])) { user =>
             onComplete(service.deposit(user.playerId, user.amount)) {
               case Success(Right(balance)) => complete(StatusCodes.OK -> Balance(balance))
-              case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> msg)
+              case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> GeneralError(msg))
 
-              case Failure(error:IllegalArgumentException) => complete(StatusCodes.BadRequest -> error.getMessage)
-              case Failure(error) => complete(StatusCodes.InternalServerError -> error)
+              case Success(_) => complete(StatusCodes.InternalServerError -> GeneralError())
+              case Failure(error:IllegalArgumentException) => complete(StatusCodes.BadRequest -> GeneralError(error.getMessage))
+              case Failure(error) => complete(StatusCodes.InternalServerError -> GeneralError(error.getMessage))
             }
           }
         } ~
@@ -58,10 +63,11 @@ class UnsecuredRoutes (service: WalletService) extends Protocols {
             onComplete(service.withdraw(user.playerId, user.amount)) {
               case Success(Right(balance)) => complete(StatusCodes.OK -> Balance(balance))
               case Success(Left(BalanceError(_, Some(balance)))) => complete(StatusCodes.BadRequest -> Balance(balance))
-              case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> msg)
+              case Success(Left(UserError(msg))) => complete(StatusCodes.BadRequest -> GeneralError(msg))
+              case Failure(error:IllegalArgumentException) => complete(StatusCodes.BadRequest -> GeneralError(error.getMessage))
 
-              case Failure(error:IllegalArgumentException) => complete(StatusCodes.BadRequest -> error.getMessage)
-              case Failure(error) => complete(StatusCodes.InternalServerError -> error.getMessage)
+              case Success(_) => complete(StatusCodes.InternalServerError -> GeneralError())
+              case Failure(error) => complete(StatusCodes.InternalServerError -> GeneralError(error.getMessage))
             }
           }
         }
